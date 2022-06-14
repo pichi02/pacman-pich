acce65087104f3c72db83b425148490b255518c8
pichi02 <lucas.pich02@gmail.com>
2022-06-13 22:28:43 -0300

    agregado de modo "casa"
    en este modo, cuando el fantasma choca con una pared se invierte su dirección para que "rebote". al desactivarse este modo se mueve al jugador a un objeto vacío que esta dentro de la casa para luego moverlo a otro que esta fuera, y, finalmente, setear una dirección aleatoria entre izquierda y derecha

---
+++ pacman/Assets/Scripts/Home.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : GhostModes
+{
+    [SerializeField] private Transform inside;
+    [SerializeField] private Transform outside;
+
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnDisable()
+    {
+
+        if (gameObject.activeInHierarchy)
+        {
+            StartCoroutine(ExitHome());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (enabled && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            ghost.movement.SetDirection(-ghost.movement.direction);
+            Debug.Log(ghost.movement.direction);
+        }
+
+    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (enabled && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+    //    {
+    //        ghost.movement.SetDirection(-ghost.movement.direction);
+    //        Debug.Log(ghost.movement.direction);
+    //    }
+    //}
+
+    private IEnumerator ExitHome()
+    {
+
+        ghost.movement.SetDirection(Vector2.up, true);
+        ghost.movement.ChangeKinematicRBValue(true);
+        ghost.movement.enabled = false;
+
+        Vector3 position = transform.position;
+
+        float duration = 0.5f;
+        float elapsed = 0f;
+
+
+        while (elapsed < duration)
+        {
+            ghost.SetPosition(Vector3.Lerp(position, inside.position, elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            ghost.SetPosition(Vector3.Lerp(inside.position, outside.position, elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+
+        ghost.movement.SetDirection(new Vector2(Random.value < 0.5f ? -1f : 1f, 0f), true);
+        ghost.movement.ChangeKinematicRBValue(false);
+        ghost.movement.enabled = true;
+    }
+    public Transform GetInside()
+    {
+        return inside;
+    }
+    public Transform GetOutside()
+    {
+        return outside;
+    }
+}
