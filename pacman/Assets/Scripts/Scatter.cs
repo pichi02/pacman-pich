ffd0248cdfae3b39f82714ea90708e60734f522f
pichi02 <lucas.pich02@gmail.com>
2022-06-13 22:20:53 -0300

    agregado de "modo dispersion"
    en este modo, los fantasmas eligen una dirección disponible aleatoria

---
+++ pacman/Assets/Scripts/Scatter.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Scatter : GhostModes
+{
+    private void OnDisable()
+    {
+        ghost.chase.Enable();
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        GhostCheckpoint checkpoint = other.GetComponent<GhostCheckpoint>();
+
+       
+        if (checkpoint != null && enabled)
+        {
+          
+            int index = Random.Range(0, checkpoint.GetPossibleDirections().Count);
+
+            
+            if (checkpoint.GetPossibleDirections()[index] == -ghost.movement.direction)
+            {
+                index++;
+
+               
+                if (index >= checkpoint.GetPossibleDirections().Count)
+                {
+                    index = 0;
+                }
+            }
+
+            ghost.movement.SetDirection(checkpoint.GetPossibleDirections()[index]);
+       
+        }
+    }
+
+}
