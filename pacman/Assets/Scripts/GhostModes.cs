05879007dbc3fef02f82197093fd9e4e15891dfa
pichi02 <lucas.pich02@gmail.com>
2022-06-13 23:02:52 -0300

    clase abstracta "GhostModes"
    esta clase contiene los metodos que activan y desactivan los scripts de los modos

---
+++ pacman/Assets/Scripts/GhostModes.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GhostModes : MonoBehaviour
+{
+    [SerializeField] protected Ghost ghost;
+    [SerializeField] protected float duration;
+
+    private void Awake()
+    {
+        ghost = GetComponent<Ghost>();  
+    }
+
+    public void Enable()
+    {
+        Enable(duration);
+       
+    }
+
+    public virtual void Enable(float duration)
+    {
+        enabled = true;
+
+        CancelInvoke();
+        Invoke(nameof(Disable), duration);
+    }
+
+    public virtual void Disable()
+    {
+        enabled = false;
+
+        CancelInvoke();
+    }
+}
